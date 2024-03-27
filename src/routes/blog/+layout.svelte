f2830bd536f2a695c741ed176469d8649b1304bf
Daiki48 <daiki48.engineer@gmail.com>
2024-03-28 01:51:20 +0900

    :zap: Added content and Update posts style.

---
+++ src/routes/blog/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import * as config from '$lib/config';
+</script>
+
+<svelte:head>
+	<title>{config.title}</title>
+</svelte:head>
+<slot />
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,13 +1,8 @@
 <script lang="ts">
 	import { formatDate } from '$lib/utils';
-	import * as config from '$lib/config';
 	import type { PageData } from './$types';
 	export let data: PageData;
 </script>
-
-<svelte:head>
-	<title>{config.title}</title>
-</svelte:head>
 
 <h1>記事一覧</h1>
 
@@ -18,7 +13,6 @@
 				<li class="post">
 					<h1>{post.title}</h1>
 					<div class="middle-container">
-						<p class="tags">{post.tags}</p>
 						<div class="date-container">
 							{#if post.updatedAt}
 								<p class="update-date">更新日 : {formatDate(post.updatedAt)}</p>
@@ -26,8 +20,8 @@
 							<p class="create-date">公開日 : {formatDate(post.createdAt)}</p>
 						</div>
 					</div>
-					<!-- <p class="category">{post.category}</p> -->
 					<p class="description">{post.description}</p>
+					<p class="tags">{post.tags}</p>
 				</li>
 			</a>
 		{/each}
@@ -35,20 +29,25 @@
 </section>
 
 <style>
+	ul {
+		margin: 0;
+		padding: 0;
+	}
 	.posts {
-		display: flex;
-		flex-direction: column;
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(min(240px, 100%), 1fr));
+		gap: 4rem 3rem;
 		list-style: none;
+		width: 100%;
 		padding: 0;
 		margin: 0;
 	}
 	.post {
-		/* border: 1px #333 solid; */
 		border-radius: 10px;
 		box-shadow: 2px 2px 4px gray;
 		padding: 10px;
-		margin: 10px;
 		width: 100%;
+		height: 100%;
 	}
 
 	.post:hover {
@@ -69,9 +68,6 @@
 		justify-content: space-between;
 	}
 
-	.middle-container .tags {
-		justify-content: start;
-	}
 	.middle-container .date-container {
 		justify-content: end;
 	}

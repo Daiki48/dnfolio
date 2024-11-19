e217f6682ad4225b2e1ed22e97f7171a38478fdb
Daiki48 <daiki48.engineer@gmail.com>
2024-11-20 00:14:51 +0900

    :art: Adjusted img

--- src/_styles/global.ts
+++ src/_styles/global.ts
@@ -15,11 +15,16 @@ export const globalStyle = `
 
 	code {
 		padding: 6px;
-		margin: 0 4px;
+		margin: 2px 4px;
 		background-color: #e4e7ed;
 		color: #cf0635;
 		border-radius: 4px;
 		font-weight: bold;
+	}
+
+	img {
+		max-width: 80%;
+		height: auto;
 	}
 
 	blockquote {

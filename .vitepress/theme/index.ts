577eed44a64a1435d07c2763b80b0fd008d78e5a
Daiki48 <daiki48.engineer@gmail.com>
2024-11-24 22:56:29 +0900

    :zap: Added title, description, tags in the doc layout

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,12 +2,16 @@
 import { h } from "vue";
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
+import BlogHeader from "./BlogHeader.vue";
 import "./style.css";
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
+			'doc-before': () => {
+				return h(BlogHeader)
+			},
     });
   },
   enhanceApp({ app, router, siteData }) {

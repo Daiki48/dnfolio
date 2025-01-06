bb4b6e519323ee32e5745e3027a67e018b7f7b30
Daiki48 <daiki48.engineer@gmail.com>
2025-01-06 23:03:39 +0900

    :coffin: Not working

--- blog_builder/src/main.rs
+++ blog_builder/src/main.rs
@@ -1,3 +1,30 @@
+use serde::Serialize;
+use std::fs;
+use std::path::Path;
+
+#[derive(Serialize)]
+struct Report {
+    text: String,
+    link: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let path = Path::new("data/en/daily-report/list.json");
+
+    if let Some(parent) = path.parent() {
+        if !parent.exists() {
+            fs::create_dir_all(parent).expect("Failed to create directories");
+        }
+    };
+
+    let report = Report {
+        text: "Daily Report".to_string(),
+        link: "This is a sample daily report json file".to_string(),
+    };
+
+    let json_data = serde_json::to_string_pretty(&report).expect("Failed to serialize data");
+
+    fs::write(path, json_data).expect("Failed to write data to file");
+
+    println!("JSON file created at {:?}", path);
 }
